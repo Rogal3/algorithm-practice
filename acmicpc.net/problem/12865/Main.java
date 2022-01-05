c6fdc0079622d8dbbcc554be62048e368ffa6c8c
Rogal3 <cws0188@gmail.com>
2022-01-06 02:34:13 +0900

    https://www.acmicpc.net/problem/12865

---
+++ acmicpc.net/problem/12865/Main.java
@@ -0,0 +1,92 @@
+import java.io.*;
+import java.util.*;
+
+/**
+ * @see https://www.acmicpc.net/problem/12865
+ * @author rogal
+ */
+
+public class Main {
+    static FastIO io;
+
+    public static void main(String[] args) {
+        io = FastIO.of(args);
+
+        int n = io.nextInt(), k = io.nextInt();
+        int[] maxValueByWeightSum = new int[k + 1];
+        for (int i = 0; i < n; i++) {
+            int w = io.nextInt();
+            int v = io.nextInt();
+            for (int weightSum = k - w; weightSum >= 0; weightSum--) {
+                if (weightSum == 0 || maxValueByWeightSum[weightSum] != 0) {
+                    maxValueByWeightSum[weightSum + w] = Math.max(maxValueByWeightSum[weightSum + w], maxValueByWeightSum[weightSum] + v);
+                }
+            }
+        }
+        int maxValue = 0;
+        for (int weightSum = 0; weightSum <= k; weightSum++) {
+            maxValue = Math.max(maxValue, maxValueByWeightSum[weightSum]);
+        }
+        io.print(maxValue);
+
+        io.flush();
+    }
+
+    static class FastIO extends PrintWriter {
+        private static final int bufferSize = 1 << 16;
+        private InputStream in;
+
+        private FastIO(InputStream in, boolean autoFlush) {
+            super(new BufferedWriter(new OutputStreamWriter(System.out), bufferSize), autoFlush);
+            this.in = new BufferedInputStream(in, bufferSize);
+        }
+
+        static FastIO of(String[] args) {
+            if (Arrays.asList(args).contains("--local")) {
+                try {
+                    return new FastIO(new FileInputStream("input.txt"), true);
+                } catch (Exception e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return new FastIO(System.in, false);
+        }
+
+        String next() {
+            StringBuilder stringBuilder = new StringBuilder();
+            int b;
+            try {
+                while (isDelimiter(b = in.read()));
+                do {
+                    stringBuilder.append((char) b);
+                } while (!isDelimiter(b = in.read()));
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+            return stringBuilder.toString();
+        }
+
+        int nextInt() {
+            int mag = 0;
+            int sign = 1;
+            int b;
+            try {
+                while (isDelimiter(b = in.read()));
+                if (b == '-') {
+                    sign = -1;
+                    b = in.read();
+                }
+                do {
+                    mag = mag * 10 + b - '0';
+                } while (!isDelimiter(b = in.read()));
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+            return sign * mag;
+        }
+
+        boolean isDelimiter(int c) {
+            return c == ' ' || c == '\t' || c == '\n' || c == '\r' || c == '\f' || c == -1;
+        }
+    }
+}
